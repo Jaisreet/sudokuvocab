20d513d31ea51bde1234d94b2e88190cea754e7b
Jaisreet Khaira <jaisreetkhaira@Jaisreets-MacBook-Air.local>
2023-02-22 13:18:15 -0800

    updated the pictures

--- app/src/main/java/com/example/sudokuapp/MainActivity.java
+++ app/src/main/java/com/example/sudokuapp/MainActivity.java
@@ -6,14 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private SudokuBoard gameBoard;
     private boardFill gameBoardFill;
 
+    private Game_input input;
+
     private Button hint;
     private Button erase;
+
+    private ImageView reset;
+
+    private ImageButton settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +48,20 @@ public class MainActivity extends AppCompatActivity {
                 eraseText();
             }
         });
+
+        reset = (ImageView) findViewById(R.id.resetbtn);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
     }
     public void backToMain(View view){
-        Intent intent = new Intent(this, First_page.class);
+        Intent intent = new Intent(this, MainActivity.class);
         this.startActivity(intent);
     }
 
-    public void settingPage(View view){
-        Intent intent = new Intent(this, setting_page.class);
-        this.startActivity(intent);
-    }
 
     public void BTNOnePress(View view) {
         gameBoardFill.setNumberPos(1);
@@ -98,6 +110,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void eraseText() {
         gameBoardFill.eraseNumber();
+    }
+
+    public void reset() {
+       boardFill board= new boardFill();
+
     }
     public void openDialog() {
         hintDialog hint = new hintDialog();
