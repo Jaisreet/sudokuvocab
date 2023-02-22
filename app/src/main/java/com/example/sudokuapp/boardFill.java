0f6df41df18958f367bfb66862f1860f2927db9c
jka296 <jka296@sfu.ca>
2023-02-21 17:43:18 -0800

    input class added

--- app/src/main/java/com/example/sudokuapp/boardFill.java
+++ app/src/main/java/com/example/sudokuapp/boardFill.java
@@ -5,11 +5,8 @@ import static java.lang.System.*;
 import java.util.ArrayList;
 
 public class boardFill {
-
-
     int[][] board;
-
-    private Game_input input = new Game_input();
+    Game_input input = new Game_input();
     ArrayList<ArrayList<Object>> emptyBoxIndex;
     int selected_row;
     int selected_column;
@@ -19,42 +16,18 @@ public class boardFill {
         // when the user has not selected a square yet, set selected col and row to -1
         selected_column = -1;
         selected_row = -1;
-<<<<<<< HEAD
-        board = new int[9][9];
-        flag = new int[9][9];
-=======
-
-
-        input = new int[][]{
-                {0, 0, 1, 7, 0, 0, 5, 0, 9},
-                {5,7,3, 0, 2, 4, 1, 0, 6},
-                {8, 0, 0, 5, 0, 1, 0, 0, 2},
-                {7, 0, 0, 2, 9, 5, 0, 1, 8},
-                {0, 0, 9, 4, 0, 0, 3, 0, 5},
-                {6, 5, 2, 8, 0, 0, 0, 0,7},
-                {4, 6, 5, 0, 8, 0, 0, 7, 1},
-                {0, 0, 0, 1, 5, 9, 0, 0, 4},
-                {9, 0, 8, 0, 0, 7, 0, 5, 3}};
-
         board = new int[9][9];  // main working board
         flag = new int[9][9];   // flag to keep track of pre-filled squares
 
         // algorithm to move generated board set up into main board
 
         // for every row
->>>>>>> 751a4004f824cd63a77063cdc541de02629dd7c5
         for(int r=0; r<9; r++) {
             // for every colomn
             for(int c=0;c<9;c++) {
-<<<<<<< HEAD
                 board[r][c] = input.getInput()[r][c];
-=======
-
-                // copy the value from input to main board
-                board[r][c] = input[r][c];
 
                 // if the board at that spot is not empty, set the flag to one
->>>>>>> 751a4004f824cd63a77063cdc541de02629dd7c5
                 if(board[r][c] != 0){
                     flag[r][c] = 1;
                 }
