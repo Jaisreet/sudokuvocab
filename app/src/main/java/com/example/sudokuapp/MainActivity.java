b67cc80aea693694fd7e58bc29d12559b630d27e
Jaisreet Khaira <jaisreetkhaira@MacBook-Air.localdomain>
2023-02-22 18:09:28 -0800

    added dialog box for settings

--- app/src/main/java/com/example/sudokuapp/MainActivity.java
+++ app/src/main/java/com/example/sudokuapp/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.sudokuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
     private Button erase;
 
     private ImageView reset;
+    private ImageView settingsDialog;
 
-    private ImageButton settings;
+    private settingsDialog dialog;
+
+    private Button resume;
+    private Button settings;
+    private Button newGame;
+    private Button quitGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 openDialog();
             }
         });
+
         erase = (Button) findViewById(R.id.erase);
         erase.setOnClickListener(new View.OnClickListener() {
 
@@ -54,6 +62,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 reset();
+            }
+        });
+
+
+        settingsDialog = (ImageView) findViewById(R.id.settingsDialog);
+        settingsDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               openSettingDialog();
+               System.out.println("Hello from dialog");
             }
         });
     }
@@ -119,5 +137,10 @@ public class MainActivity extends AppCompatActivity {
     public void openDialog() {
         hintDialog hint = new hintDialog();
         hint.show(getSupportFragmentManager(), "hintDialog");
+    }
+
+    public void openSettingDialog() {
+        settingsDialog settings = new settingsDialog();
+        settings.show(getSupportFragmentManager(), "Settings");
     }
 }
---
+++ app/src/main/java/com/example/sudokuapp/settingsDialog.java
@@ -0,0 +1,23 @@
+package com.example.sudokuapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class settingsDialog extends AppCompatDialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.settings_dialog, null));
+
+        return builder.create();
+    }
+
+}
