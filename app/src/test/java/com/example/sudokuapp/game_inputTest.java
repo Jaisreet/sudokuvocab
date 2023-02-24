dadcc3c414a73e4df6a18951d9a5f529d770a137
Christine <lhn7@sfu.ca>
2023-02-23 21:37:15 -0800

    complete unit Test for game_input

--- app/src/test/java/com/example/sudokuapp/game_inputTest.java
+++ app/src/test/java/com/example/sudokuapp/game_inputTest.java
@@ -4,43 +4,153 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import com.example.sudokuapp.Game_input;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class game_inputTest {
     Game_input gameInput = new Game_input();
 
     @Test
     public void gridCopyTest() {
-
+        int[][] arr1 = new int[9][9];
+        int[][] arr2 = new int[9][9];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                arr1[i][j] = i+j+1;
+            }
+        }
+        gameInput.gridCopy(arr1, arr2, 9);
+        //Check if arr2 is equal to arr1
+        for(int i = 0; i <9; i++){
+            for(int j = 0; j < 9; j++){
+                assertEquals(arr1[i][j], arr2[i][j]);
+            }
+        }
     }
 
     @Test
     public void getSolutionTest() {
-
+        assertArrayEquals(gameInput.solution, gameInput.getSolution());
     }
 
     @Test
     public void fillDiagonalTest(){
+        int[][] arr = new int[9][9];
+        gameInput.fillDiagonal(arr);
 
+        // Check that each diagonal box has distinct numbers
+        for (int i = 0; i < 9; i += 3) {
+            int[] boxValues = new int[9];
+            int k = 0;
+            for (int j = 0; j < 3; j++) {
+                for (int l = 0; l < 3; l++) {
+                    int value = arr[i+j][i+l];
+                    assertTrue(value >= 1 && value <= 9);
+                    assertFalse(boxValues[value-1] == value);
+                    boxValues[k++] = value;
+                }
+            }
+        }
     }
 
     @Test
     public void randomNumTest(){
+        int[] RandomResult = {0,0,0,0,0};
+        for(int i = 0; i < 5; i++){
+            RandomResult[i] = gameInput.randomNum(1);
+        }
+        for(int i = 1; i < 5; i++){
+            assertTrue(RandomResult[0] != RandomResult[i]);
+        }
 
     }
 
     @Test
     public void unUsedInBoxTest(){
+        int[][] arr = {{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 2, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 3, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 4, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 6, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 7, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 9, 0, 0, 0, 0, 0, 0, 0, 0 }};
 
+        int rowStart = 1;
+        int colStart = 1;
+        int num = 1;
+
+        // Call unUsedInBox function
+        boolean result = gameInput.unUsedInBox(rowStart, colStart, num);
+
+        // Check that the result is true
+        assertTrue(result);
+
+        // Set one of the values in the box to the number we're checking for
+        arr[rowStart+1][colStart+1] = num;
+
+        // Call unUsedInBox function again
+        result = gameInput.unUsedInBox(rowStart, colStart, num);
+
+        // Check that the result is false
+        assertFalse(result);
     }
 
     @Test
     public void fillBoxTest(){
+        int arr[][] = {{ 3, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
+        gameInput.fillBox(0,0);
 
+        // Check that all values in the box are between 1 and 9
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertTrue(arr[i][j] >= 1 && arr[i][j] <= 9);
+            }
+        }
+
+        // Check that all values in the box are unique
+        Set<Integer> uniqueValues = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                uniqueValues.add(arr[i][j]);
+            }
+        }
+        assertEquals(9, uniqueValues.size());
     }
 
     @Test
-    public void fillRemainingTest(){}
+    public void fillRemainingTest() {
+        int[][] arr = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
+        arr[0][1] = 1;
+        arr[3][3] = 5;
+        arr[7][8] = 9;
+
+        assertTrue(gameInput.fillRemaining(0, 0));
+        int sumOfFirstRow = 0;
+        for(int i = 0; i < 9; i++){
+            sumOfFirstRow += arr[0][i];
+        }
+        assertEquals(45, sumOfFirstRow); // Check the sum of the first row
+    }
     @Test
     public void CheckIfSafeTest(){
         int[][] arr = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
